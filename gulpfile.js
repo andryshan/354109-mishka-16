c19d84afdfe774e7bf4743482ba35d5b4bdde7c2
andrey <avolkov123@mail.ru>
2019-04-26 01:57:11 +0300

    Собрали спрайт плагином svgstore, заинлайнили в файлы .html плагинами posthtml и posthtml-include, удалил fill из svg, которые идут в спрайт, чтобы корректно отображались

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,9 @@ var csso = require("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -41,6 +44,23 @@ gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("source"));
 });
 
 gulp.task("server", function () {
